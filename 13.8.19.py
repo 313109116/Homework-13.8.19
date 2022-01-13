df65b35690fd599639f23a0006c2322ede40bb69
313109116 <94843219+313109116@users.noreply.github.com>
2022-01-13 19:34:11 +0700

    Add files via upload

---
+++ 13.8.19.py
@@ -0,0 +1,16 @@
+tickets = int(input("Сколько билетов вам необходимо? \n"))
+price = []
+for i in range(1, tickets + 1):
+    age = int(input(f"Возраст {i} участника? "))
+    if age < 18:
+        price.append(0)
+    if age > 25:
+        price.append(1390)
+    elif age >= 18 <= 25:
+        price.append(990)
+        # print(price)
+
+if tickets > 3:
+        print("Сумма с учетом скидки составляет ", ((sum(price) * 0.9)), "руб.")
+else:
+        print("Сумма составляет ", sum(price), "руб.")
